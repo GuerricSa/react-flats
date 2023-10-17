9a631d633952a87a0c4c51abb35ec8bb49a57f4b
guerric sant <guerric.sant@gmail.com>
2023-10-17 15:12:17 +0200

    creating flats and adding google map

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,24 +1,44 @@
 import React, { Component } from 'react';
 import FlatList from './flat-list';
-import Map from './map';
-import flats from '../data/flats';
+import flats from '../../data/flats';
+import GoogleMapReact from 'google-map-react';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      flats: [flats]
+      selectedFlat: flats[0],
+      flats
     };
   }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
+  // selectFlat = () => {
+  //   this.setState({ selectedFlat: flats[index]});
+  // }
+
   render() {
     return (
       <div>
         <div className="left-scene">
-          <FlatList />
+          <FlatList
+            flats={this.state.flats}
+            // selectFlat={this.selectFlat}
+            // selectedFlat={this.state.selectedFlat}
+          />
         </div>
-        <div className="right-scene">
-          <Map />
+        <div className="right-scene" style={{ height: '100vh', width: '100%' }}>
+          <GoogleMapReact
+            defaultCenter={this.center}
+            defaultZoom={11}
+          />
         </div>
       </div>
     );
--- src/components/map.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Map extends Component {
-  render() {
-
-  }
-}
-
-export default Map;
